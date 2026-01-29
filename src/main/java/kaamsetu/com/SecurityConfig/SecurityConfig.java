7ee6a7366854c1844323964a0d7c63002bede4e8
nitishyadav78 <yadavnitish896@gmail.com>
2026-01-29 14:38:52 +0530

    commit

--- src/main/java/kaamsetu/com/SecurityConfig/SecurityConfig.java
+++ src/main/java/kaamsetu/com/SecurityConfig/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig
 {
+    //final
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
     {
