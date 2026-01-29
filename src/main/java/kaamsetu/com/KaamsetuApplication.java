1cbbf596bb9b26f8dbf468316f7299137998a476
nitishyadav78 <yadavnitish896@gmail.com>
2026-01-29 14:10:01 +0530

    LocalFirstCommitTillJobRepository

---
+++ src/main/java/kaamsetu/com/KaamsetuApplication.java
@@ -0,0 +1,14 @@
+package kaamsetu.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaamsetuApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(KaamsetuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kaamsetu/com/KaamsetuApplicationTests.java
@@ -0,0 +1,13 @@
+package kaamsetu.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaamsetuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
